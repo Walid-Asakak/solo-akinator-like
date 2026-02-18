0cdc0aeafe7fbe3e35d60434d978c0944a6f8039
Walid Asakak <asakakwalid@gmail.com>
2026-02-18 02:21:38 +0000

    feat(index): Creation of the index page with his style and adding an image in the index page

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+session_start();
+
+// Integration of the layout & template : 
+$layoutTitle = 'Accueil | Akinator Jeu';
+$template = 'templates/index.phtml';
+include 'layout/layout.phtml';
